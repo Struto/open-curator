d655f597ae534c693059b7cc0373154cb8d0f578
Archie Makuwa <archie@aatsol.co.za>
2015-10-23 12:57:11 +0200

    Updated theme, scripts and functions

--- wp-content/themes/open-curator/functions.php
+++ wp-content/themes/open-curator/functions.php
@@ -18,6 +18,8 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );        // default css
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/css/custom.css' );    // child theme css
+
+    wp_enqueue_script('custom-scripts', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ) );
 }
 
 
--- wp-content/themes/open-curator/js/custom.js
+++ wp-content/themes/open-curator/js/custom.js
@@ -1,3 +1,11 @@
 /**
  * Created by Archie22is on 15/10/23.
  */
+
+jQuery( document ).ready(function($) {
+
+
+
+
+    //console.log( "ready!" );
+});
