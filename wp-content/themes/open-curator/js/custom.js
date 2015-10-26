5286667e2f0bad4512f7b677f96360e44145d3fa
Archie Makuwa <archie@aatsol.co.za>
2015-10-26 17:38:53 +0200

    Some Sliding #01

--- wp-content/themes/open-curator/js/custom.js
+++ wp-content/themes/open-curator/js/custom.js
@@ -5,7 +5,20 @@
 jQuery( document ).ready(function($) {
 
 
+    // Rotate Featured Home Page Posts
+    $(".slides > div:gt(0)").hide();
+
+    setInterval(function() {
+        $('.slides > li:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('.slides');
+    },  3000);
 
 
-    //console.log( "ready!" );
+
+
+
 });
