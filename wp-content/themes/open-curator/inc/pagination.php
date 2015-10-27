670c14223ae90a9237a4df26a928934948203fa2
Archie Makuwa <archie@aatsol.co.za>
2015-10-27 15:58:06 +0200

    Revison from the man #01

--- wp-content/themes/open-curator/inc/pagination.php
+++ wp-content/themes/open-curator/inc/pagination.php
@@ -1,10 +1,14 @@
 <nav class="pagination group">
-	<?php if ( function_exists('wp_pagenavi') ): ?>
-		<?php wp_pagenavi(); ?>
-	<?php else: ?>
-		<ul class="group">
-			<li class="prev left"><?php previous_posts_link(); ?></li>
-			<li class="next right"><?php next_posts_link(); ?></li>
-		</ul>
-	<?php endif; ?>
+    <?php if (!is_home() ) : ?>
+
+        <?php if ( function_exists('wp_pagenavi') ): ?>
+            <?php wp_pagenavi(); ?>
+        <?php else: ?>
+            <ul class="group">
+                <li class="prev left"><?php previous_posts_link(); ?></li>
+                <li class="next right"><?php next_posts_link(); ?></li>
+            </ul>
+        <?php endif; ?>
+
+    <?php endif; ?>
 </nav><!--/.pagination-->
