733d32cf420887a9a92e55da141393e1cf3eb7a2
Archie Makuwa <archie@aatsol.co.za>
2015-10-27 12:52:50 +0200

    test script

---
+++ wp-content/themes/open-curator/index.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+
+    <section class="content">
+
+        <?php get_template_part('inc/page-title'); ?>
+
+        <div class="pad group">
+
+            <?php get_template_part('inc/featured'); ?>
+
+            <?php if ( have_posts() ) : ?>
+
+                <div class="post-list group">
+                    <?php $i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
+                        <?php get_template_part('content'); ?>
+                        <?php if($i % 2 == 0) { echo '</div><div class="post-row">'; } $i++; endwhile; echo '</div>'; ?>
+                </div><!--/.post-list-->
+
+                <?php get_template_part('inc/pagination'); ?>
+
+
+                <!-- Homepage Addional Widgets -->
+                <?php
+
+                    //if ( function_exists() ):
+                        if ( is_home() ) {
+                            dynamic_sidebar( 'homebotttomarea' );
+                        } else {
+                            echo "No wiget";
+                        }
+
+                    //endif;
+
+
+                ?>
+                <h3>Did something appear above?</h3>
+                <!-- Homepage end of additional widgets -->
+
+
+            <?php endif; ?>
+
+        </div><!--/.pad-->
+
+    </section><!--/.content-->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
