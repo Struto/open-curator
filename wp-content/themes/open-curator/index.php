c014cd94548ac2f2d831a851cfa18e7ef5bb2ab8
Archie Makuwa <archie@aatsol.co.za>
2015-10-28 10:41:45 +0200

    Some odd test hacks #01

--- wp-content/themes/open-curator/index.php
+++ wp-content/themes/open-curator/index.php
@@ -11,56 +11,46 @@
             <?php if ( have_posts() ) : ?>
 
                 <?php
-
                     // show something else if home page
-                    if ( is_home() ) {  //return filtered loop  ?>
-
-                        <?php
-                            $my_query = new WP_Query( array(
-                                    'post_type' => 'post',
-                                    'post_status' => 'publish',
-                                    'tax_query' => array(
-                                        array(
-                                            'taxonomy' => 'category',
-                                            'field'    => 'slug',
-                                            'terms'    => array('featured'),
-                                        ),
+                    if ( is_home() ) {  //return filtered loop
+                        $paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+                        $my_query = new WP_Query( array(
+                                'post_type' => 'post',
+                                'post_status' => 'publish',
+                                'posts_per_page' => 4,
+                                'page' => $paged,
+                                'tax_query' => array(
+                                    array(
+                                        'taxonomy' => 'category',
+                                        'field'    => 'slug',
+                                        'terms'    => array('featured'),
                                     ),
-                                )
-                            );
+                                ),
+                            )
+                        );
+                    ?>
 
+                    <div class="post-list group">
+                        <?php //$i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
+                        <?php $i = 1; echo '<div class="post-row">'; while ( $my_query->have_posts() ): $my_query->the_post(); ?>
 
+                            <?php get_template_part('content'); ?>
+                            <?php if($i % 2 == 0) { echo '</div><div class="post-row">'; } $i++; endwhile; echo '</div>'; ?>
+                    </div><!--/.post-list-->
 
-                        ?>
-
-
-
-                        <div class="post-list group">
-                            <?php //$i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
-                            <?php $i = 1; echo '<div class="post-row">'; while ( $my_query->have_posts() ): $my_query->the_post(); ?>
-
-                                <?php get_template_part('content'); ?>
-                                <?php if($i % 2 == 0) { echo '</div><div class="post-row">'; } $i++; endwhile; echo '</div>'; ?>
-                        </div><!--/.post-list-->
-
-                        <!-- Custom Homepage Bottom Widget Area-->
-                        <div class="home-bottom">
-                        <?php
-                            dynamic_sidebar( 'homebotttomarea' );
-                        } else {
-                            echo " ";
-                        } ?>
-                        </div>
+                    <!-- Custom Homepage Bottom Widget Area-->
+                    <div class="home-bottom">
+                    <?php
+                        dynamic_sidebar( 'homebotttomarea' );
+                    } else {
+                        echo " ";
+                    } ?>
+                    </div>
                     <!-- Homepage end of additional widgets -->
                 <?php //endif; ?>
 
 
-                <?php else :
-
-                    // show something else if not home page
-
-
-                ?>
+                <?php else :  // show something else if not home page ?>
                 <div class="post-list group">
                     <?php $i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
                         <?php get_template_part('content'); ?>
