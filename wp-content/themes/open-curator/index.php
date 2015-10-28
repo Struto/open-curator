af7278bfb9c4c6260e6c04a1d3ca3765dda16609
Archie Makuwa <archie@aatsol.co.za>
2015-10-28 10:32:19 +0200

    Some odd test hacks #01

--- wp-content/themes/open-curator/index.php
+++ wp-content/themes/open-curator/index.php
@@ -39,7 +39,7 @@
                             <?php //$i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
                             <?php $i = 1; echo '<div class="post-row">'; while ( $my_query->have_posts() ): $my_query->the_post(); ?>
 
-                                <?php //get_template_part('content'); ?>
+                                <?php get_template_part('content'); ?>
                                 <?php if($i % 2 == 0) { echo '</div><div class="post-row">'; } $i++; endwhile; echo '</div>'; ?>
                         </div><!--/.post-list-->
 
