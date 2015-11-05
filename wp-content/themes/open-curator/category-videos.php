909d192a61b08241cd3f6c5285a4154754f7c137
Archie Makuwa <archie@aatsol.co.za>
2015-11-05 13:02:00 +0200

    Added content to the featured video section #02

--- wp-content/themes/open-curator/category-videos.php
+++ wp-content/themes/open-curator/category-videos.php
@@ -9,73 +9,76 @@
         <!-- Featured external YouTube videos -->
         <div class="feat-vids pad">
 
-            <h2 class="video-widgettitle"><?php printf( __( "'Nature is Speaking': Will we listen?", 'curation-hue' ) ); ?></h2>
+            <div class="entry">
+                <h2 class="video-widgettitle"><?php printf( __( "'Nature is Speaking': Will we listen?", 'curation-hue' ) ); ?></h2>
 
-            <p>In a recent video series from Conservation International, A-list actors including Julia Roberts,
-                Harrison Ford and Kevin Spacey personify nature. The aim of the series is to make it clear to
-                everyone that the planet has been evolving for millions of years and will continue to do so,
-                with or without humans. It is our choice to address environmental issues and change our ways
-                to ensure we play a part in its future.</p>
-            <br>
-            <div class="videos-container">
+                <p>In a recent video series from Conservation International, A-list actors including Julia Roberts,
+                    Harrison Ford and Kevin Spacey personify nature. The aim of the series is to make it clear to
+                    everyone that the planet has been evolving for millions of years and will continue to do so,
+                    with or without humans. It is our choice to address environmental issues and change our ways
+                    to ensure we play a part in its future.</p>
+                <br>
+                <div class="videos-container">
 
-                <div id="part-one" class="work">
-                    <div id="harrison-ford" class="box col4">
-                        <a class="various fancybox.iframe" href="http://www.youtube.com/embed/rM6txLtoaoc?autoplay=1">
-                            <h2>Harrison Ford is</h2>
-                            <h1>The Ocean</h1>
-                            <i class="fa fa-play"></i>
-                        </a>
+                    <div id="part-one" class="work">
+                        <div id="harrison-ford" class="box col4">
+                            <a class="various fancybox.iframe" href="http://www.youtube.com/embed/rM6txLtoaoc?autoplay=1">
+                                <h2>Harrison Ford is</h2>
+                                <h1>The Ocean</h1>
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>
+                        <div id="kevin-spacey" class="box col2">
+                            <a class="various fancybox.iframe" href="http://www.youtube.com/embed/jBqMJzv4Cs8?autoplay=1">
+                                <h2>Kevin Spacey is</h2>
+                                <h1>The Rainforest</h1>
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>
+                        <div id="ian-somerhalder" class="box col2">
+                            <a class="various fancybox.iframe" href="http://www.youtube.com/embed/lVMV3StvLCs?autoplay=1">
+                                <h2>Ian Somerhalder is</h2>
+                                <h1>Coral Reef</h1>
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>
+                        <div id="lupita-nyongo" class="box col2">
+                            <a class="various fancybox.iframe" href="http://www.youtube.com/embed/0_OxI2JZex4?autoplay=1">
+                                <h2>Lupita Nyong'o is</h2>
+                                <h1>Flower</h1>
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>
+                        <div id="edward-norton" class="box col2">
+                            <a class="various fancybox.iframe" href="http://www.youtube.com/embed/Dor4XvjA8Wo?autoplay=1">
+                                <h2>Edward Norton is</h2>
+                                <h1>Soil</h1>
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>
+                        <div id="julia-roberts" class="box col4">
+                            <a class="various fancybox.iframe" href="http://www.youtube.com/embed/WmVLcj-XKnM?autoplay=1">
+                                <h2>Julia Roberts is</h2>
+                                <h1>Mother Nature</h1>
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>
+                        <div id="edward-norton" class="box col2">
+                            <a class="various fancybox.iframe" href="http://www.youtube.com/embed/Dor4XvjA8Wo?autoplay=1">
+                                <h2>Edward Norton is</h2>
+                                <h1>Soil</h1>
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>
+                        <div id="penelope-cruz" class="box col2">
+                            <a class="various fancybox.iframe" href="http://www.youtube.com/embed/fwV9OYeGN88?autoplay=1">
+                                <h2>Penelope Cruz is</h2>
+                                <h1>Water</h1>
+                                <i class="fa fa-play"></i>
+                            </a>
+                        </div>
                     </div>
-                    <div id="kevin-spacey" class="box col2">
-                        <a class="various fancybox.iframe" href="http://www.youtube.com/embed/jBqMJzv4Cs8?autoplay=1">
-                            <h2>Kevin Spacey is</h2>
-                            <h1>The Rainforest</h1>
-                            <i class="fa fa-play"></i>
-                        </a>
-                    </div>
-                    <div id="ian-somerhalder" class="box col2">
-                        <a class="various fancybox.iframe" href="http://www.youtube.com/embed/lVMV3StvLCs?autoplay=1">
-                            <h2>Ian Somerhalder is</h2>
-                            <h1>Coral Reef</h1>
-                            <i class="fa fa-play"></i>
-                        </a>
-                    </div>
-                    <div id="lupita-nyongo" class="box col2">
-                        <a class="various fancybox.iframe" href="http://www.youtube.com/embed/0_OxI2JZex4?autoplay=1">
-                            <h2>Lupita Nyong'o is</h2>
-                            <h1>Flower</h1>
-                            <i class="fa fa-play"></i>
-                        </a>
-                    </div>
-                    <div id="edward-norton" class="box col2">
-                        <a class="various fancybox.iframe" href="http://www.youtube.com/embed/Dor4XvjA8Wo?autoplay=1">
-                            <h2>Edward Norton is</h2>
-                            <h1>Soil</h1>
-                            <i class="fa fa-play"></i>
-                        </a>
-                    </div>
-                    <div id="julia-roberts" class="box col4">
-                        <a class="various fancybox.iframe" href="http://www.youtube.com/embed/WmVLcj-XKnM?autoplay=1">
-                            <h2>Julia Roberts is</h2>
-                            <h1>Mother Nature</h1>
-                            <i class="fa fa-play"></i>
-                        </a>
-                    </div>
-                    <div id="edward-norton" class="box col2">
-                        <a class="various fancybox.iframe" href="http://www.youtube.com/embed/Dor4XvjA8Wo?autoplay=1">
-                            <h2>Edward Norton is</h2>
-                            <h1>Soil</h1>
-                            <i class="fa fa-play"></i>
-                        </a>
-                    </div>
-                    <div id="penelope-cruz" class="box col2">
-                        <a class="various fancybox.iframe" href="http://www.youtube.com/embed/fwV9OYeGN88?autoplay=1">
-                            <h2>Penelope Cruz is</h2>
-                            <h1>Water</h1>
-                            <i class="fa fa-play"></i>
-                        </a>
-                    </div>
+
                 </div>
 
             </div>
