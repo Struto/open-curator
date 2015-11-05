4179f01c73fabeb50ba5c698ce3946115f749961
Archie Makuwa <archie@aatsol.co.za>
2015-11-05 09:51:39 +0200

    Updating the videos page

--- wp-content/themes/open-curator/js/custom.js
+++ wp-content/themes/open-curator/js/custom.js
@@ -4,17 +4,21 @@
 
 jQuery( document ).ready(function($) {
 
-    //
-    $(".various").fancybox({
-        maxWidth	: 800,
-        maxHeight	: 600,
-        fitToView	: false,
-        width		: '70%',
-        height		: '70%',
-        autoSize	: false,
-        closeClick	: false,
-        openEffect	: 'none',
-        closeEffect	: 'none'
+    // Featured videos fancybox
+    $('.various').click( function() {
+        //$(".various").fancybox({
+        $.fancybox({
+            maxWidth	: 800,
+            maxHeight	: 600,
+            fitToView	: false,
+            width		: '70%',
+            height		: '70%',
+            autoSize	: false,
+            closeClick	: false,
+            openEffect	: 'none',
+            closeEffect	: 'none'
+        });
+        return false;
     });
 
 
