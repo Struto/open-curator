189b5370fe7bd0803caf20f6d8d456b31aa94bb5
Archie Makuwa <archie@aatsol.co.za>
2015-12-01 11:27:01 +0200

    Hack 01

--- wp-content/themes/open-curator/page-templates/cop-template.php
+++ wp-content/themes/open-curator/page-templates/cop-template.php
@@ -11,6 +11,9 @@ Template Name: COP Template
 
         <div class="pad group">
 
+            <!-- On the homepage where there is a featured piece -->
+            <?php //get_template_part('inc/featured'); ?>
+
             <?php if ( have_posts() ) : ?>
 
                 <?php
@@ -31,24 +34,41 @@ Template Name: COP Template
                             ),
                         )
                     );
-                ?>
+                    ?>
+
+                    <div class="post-list group">
+                        <?php //$i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
+                        <?php $i = 1; echo '<div class="post-row">'; while ( $my_query->have_posts() ): $my_query->the_post(); ?>
+
+                            <?php get_template_part('content'); ?>
+                            <?php if($i % 2 == 0) { echo '</div><div class="post-row">'; } $i++; endwhile; echo '</div>'; ?>
+                    </div><!--/.post-list-->
 
 
+                          <!-- Custom Homepage Bottom Widget Area-->
+                    <div class="home-bottom">
+                    <?php
+                    dynamic_sidebar( 'homebotttomarea' );
+                } else {
+                    echo " ";
+                } ?>
+                </div>
+                <!-- Homepage end of additional widgets -->
+                <?php //endif; ?>
 
 
+            <?php else :  // show something else if not home page ?>
                 <div class="post-list group">
-                    <?php //$i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
-                    <?php $i = 1; echo '<div class="post-row">'; while ( $my_query->have_posts() ): $my_query->the_post(); ?>
 
+                    <?php $i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
                         <?php get_template_part('content'); ?>
                         <?php if($i % 2 == 0) { echo '</div><div class="post-row">'; } $i++; endwhile; echo '</div>'; ?>
                 </div><!--/.post-list-->
 
+                <?php get_template_part('inc/pagination'); ?>
 
-            <?php else :  ?>
-                <?php echo "Empty"; ?>
+            <?php endif; ?>
 
-            <?php endif;?>
 
         </div><!--/.pad-->
 
