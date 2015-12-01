811f17a18d4c6d4a1d53cc37a6017c97b07cce1f
Archie Makuwa <archie@aatsol.co.za>
2015-12-01 11:43:50 +0200

    Hack 01

--- wp-content/themes/open-curator/page-templates/cop-template.php
+++ wp-content/themes/open-curator/page-templates/cop-template.php
@@ -60,19 +60,6 @@ Template Name: COP Template
 
 
 
-                <!--
-                <?php else :  // show something else if not home page ?>
-                    <div class="post-list group">
-
-                        <?php $i = 1; echo '<div class="post-row">'; while ( have_posts() ): the_post(); ?>
-                            <?php get_template_part('content'); ?>
-                            <?php if($i % 2 == 0) { echo '</div><div class="post-row">'; } $i++; endwhile; echo '</div>'; ?>
-                    </div><!--/.post-list-->
-
-                    <?php get_template_part('inc/pagination'); ?>
-
-                <?php endif; ?>
-                -->
 
         </div><!--/.pad-->
 
